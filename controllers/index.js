f9d540d5a2c1e821d6b16fb32df7dab0f1b60194
mlbarre <mlbarrera1538@gmail.com>
2022-02-10 20:07:30 -0600

    added index.js to api folder and included comment-routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const userRoutes = require('./user-routes.js');
 
-router.use('/users', userRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
