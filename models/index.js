acf77a2d8c64a3ef9226379195267e8996b43b35
mlbarre <mlbarrera1538@gmail.com>
2022-02-10 20:13:40 -0600

    added associations to models/index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,30 @@
+//import all models
 const User = require('./user.js');
 const Comment = require('./Comment.js')
 
+// create associations 
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Comment };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Comment, Post };
