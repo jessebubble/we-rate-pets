64d296fee1b1b0f9ffb84dde7a109cad0196d39e
coevpen <koinuevins@gmail.com>
2022-02-13 17:04:14 -0600

    add signup ability and cookie expiration

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
-const bcrypt = require('bcrypt'); // need to call this somewhere...will research modules
 
 const app = express(); // creates express application
 const PORT = process.env.PORT || 3001; // selects port 3001 for production environment Heroku
@@ -25,7 +24,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {}, // do we setup cookie logout after period of no activity
+    cookie: {expires: 30 * 60 * 1000}, //expires after 30 minutes
     resave: false,
     saveUnitialized: true,
     store: new SequelizeStore({
@@ -34,6 +33,19 @@ const sess = {
 };
 
 app.use(session(sess)); // connects session to Sequelize database (reference module 14.2.5)
+
+app.use(session(sesh));
+// if there is inactivity for 30 minutes, the session expires
+app.get('/session', function(req, res, next) {
+  if(req.session.views){
+    req.session.views++
+    res.end();
+  }
+  else{
+    req.session.views = 1
+    res.end();
+  }
+});
 
 app.use(routes); //turn on routes
 
