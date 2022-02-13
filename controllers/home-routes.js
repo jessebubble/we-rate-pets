b7e4c8eeb0132578effb6eab0ad53c5d1c6bd5d5
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-02-13 00:24:51 -0600

    login signup fineOne routes added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,8 +39,62 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+router.get('/login', (req, res) => { // takes user to login page
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+router.get('/signup', (req, res) => { // takes user to signup page
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+router.get('/post/:id', (req, res) => { // gets single post and comments from attributes
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'image_url',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true }); 
 
-
-
+        res.render('single-post', { 
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
