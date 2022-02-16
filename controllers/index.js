efb5882186d46db9ef9a932457c3ea286c8cc600
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-02-16 16:07:02 -0600

    cloudinary updates to server and controller index

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const cloudinary = require('cloudinary').v2;
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
@@ -12,4 +13,53 @@ router.use((req, res) => {
     res.status(404).end();
 });
 
-module.exports = router;
+const path = require('path');
+const applicationDirectory = path.dirname(require.main.filename);
+console.log('applicationDirectory', applicationDirectory)
+
+const uploadImage = async filename => {
+    filename = `${applicationDirectory}/uploads/${filename}`;
+    try {
+        const phot = await cloudinary.uploader.upload(filename, {
+            use_filename: true,
+            unique_filename: false
+        });
+        console.log('after cloudinary upload', photo)
+        //return photo;
+
+        return await cloudinary.url(photo.public_id, {
+            width: 400,
+            quality: 'auto',
+            fetch_format: 'auto',
+            secure: true
+        });
+    } catch (error) {
+        console.log('uploadImage error', JSON.stringify(error))
+        throw new Error(error);
+    }
+};
+
+const index = (req, res) => {
+    return res.render('index', {
+        intro: 'Welcome :)',
+        photo: req.app.get('photo')
+    });
+};
+
+const upload = async (req, res) => {
+    const uploadedFile = req.file.file[0];
+    const filename = uploadedFile.filename;
+    console.log('filename', filename)
+    try {
+        const photo = await uploadImage(filename);
+        console.log('photo', photo)
+        req.app.set('photo', photo);
+        return res.redirect(req.get('referer'));
+    } catch(error) {
+        console.log('upload error', JSON.stringify(error))
+    }
+};
+
+module.exports = { 
+    router, index, upload
+};
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
 
 const app = express(); // creates express application
 const PORT = process.env.PORT || 3001; // selects port 3001 for production environment Heroku
@@ -17,7 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-// app.set('views', path.join(__dirname, 'views' )); troubleshooting mysql error
+app.get('/', routes.index);
+//app.set('public', path.join(__dirname, 'public' )); //troubleshooting mysql error
 
 // connects session to Sequelize database (reference module 14.2.5)
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -34,7 +38,7 @@ const sess = {
 
 app.use(session(sess)); // connects session to Sequelize database (reference module 14.2.5)
 
-app.use(session(sess));
+//app.use(session(sess));
 // if there is inactivity for 30 minutes, the session expires
 app.get('/session', function(req, res, next) {
   if(req.session.views){
@@ -47,7 +51,29 @@ app.get('/session', function(req, res, next) {
   }
 });
 
-app.use(routes); //turn on routes
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
+if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
+  console.warn('!! cloudinary config is undefined !!');
+  console.warn('export CLOUDINARY_URL or set dotenv file');
+} else {
+}
+
+const storage = multer.diskStorage({
+  destination: function(req,file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage });
+
+//app.use(routes); //turn on routes
+app.post('/upload', upload.fields([{ name: 'file' }]), routes.upload);
+
 
 sequelize.sync({ force: false }).then(() => { // turn on connection to database and server (reference module 13.1.6)
     app.listen(PORT, () => console.log('Now Listening'));
